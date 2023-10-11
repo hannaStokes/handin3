cdd2cc2336fd2b834cd7cf8c2d38039b0050007e
Jeppe Balslev <jebal@itu.dk>
2023-10-11 11:54:48 +0200

    RegisterChittyChatServer expects odd server type

---
+++ client/client.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	//"strconv"
+	"strings"
+	"time"
+
+	// this has to be the same as the go.mod module,
+	// followed by the path to the folder the proto file is in.
+	gRPC "github.com/hannaStokes/handin3/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Same principle as in client. Flags allows for user specific arguments/values
+var clientsName = flag.String("name", "default", "Senders name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var server gRPC.ChittyChatClient   //the server
+var ServerConn *grpc.ClientConn //the server connection
+
+func main() {
+	//parse flag/arguments
+	flag.Parse()
+
+	fmt.Println("--- CLIENT APP ---")
+
+	//log to file instead of console
+	//f := setLog()
+	//defer f.Close()
+
+	//connect to server and close the connection when program closes
+	fmt.Println("--- join Server ---")
+	ConnectToServer()
+	defer ServerConn.Close()
+
+	//start the biding
+	parseInput()
+}
+
+// connect to server
+func ConnectToServer() {
+
+	//dial options
+	//the server is not using TLS, so we use insecure credentials
+	//(should be fine for local testing but not in the real world)
+	opts := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	//dial the server, with the flag "server", to get a connection to it
+	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
+	if err != nil {
+		log.Printf("Fail to Dial : %v", err)
+		return
+	}
+
+	// makes a client from the server connection and saves the connection
+	// and prints rather or not the connection was is READY
+	server = gRPC.NewChittyChatClient(conn)
+	ServerConn = conn
+	log.Println("the connection is: ", conn.GetState().String())
+}
+
+func parseInput() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Type the amount you wish to increment with here. Type 0 to get the current value")
+	fmt.Println("--------------------")
+
+	//Infinite loop to listen for clients input.
+	for {
+		fmt.Print("-> ")
+
+		//Read input into var input and any errors into err
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input) //Trim input
+
+		if !conReady(server) {
+			log.Printf("Client %s: something was wrong with the connection to the server :(", *clientsName)
+			continue
+		}
+
+		//Convert string to int64, return error if the int is larger than 32bit or not a number
+		getTimeNanoSeconds()
+	}
+}
+
+func getTimeNanoSeconds() {
+	//
+	log.Println(time.Now())
+	amount := &gRPC.SubMessage{
+		ClientName: *clientsName,
+	}
+
+	//
+	ack, err := server.Subscribe(context.Background(), amount)
+	if err != nil {
+		log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
+		log.Println(err)
+	}
+
+	//
+	log.Println(time.Unix(0, ack.NanoSeconds))
+
+}
+
+// Function which returns a true boolean if the connection to the server is ready, and false if it's not.
+func conReady(s gRPC.ChittyChatClient) bool {
+	return ServerConn.GetState().String() == "READY"
+}
+
+// sets the logger to use a log.txt file instead of the console
+func setLog() *os.File {
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
+}
---
+++ server/server.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	// "io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+	// this has to be the same as the go.mod module,
+	// followed by the path to the folder the proto file is in.
+	gRPC "github.com/hannaStokes/handin3/proto"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	gRPC.UnimplementedChittyChatServer        // You need this line if you have a server. "ChittyChat" should be the equivalent name of your service
+	name                             string // Not required but useful if you want to name your server
+	port                             string // Not required but useful if your server needs to know what port it's listening to
+
+	currentTime int64      // value that clients can increment.
+	mutex          sync.Mutex // used to lock the server to avoid race conditions.
+}
+
+// flags are used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
+// to use a flag then just add it as an argument when running the program.
+var serverName = flag.String("name", "default", "Senders name") // set with "-name <name>" in terminal
+var port = flag.String("port", "5400", "Server port")           // set with "-port <port>" in terminal
+
+func main() {
+
+	// f := setLog() //uncomment this line to log to a log.txt file instead of the console
+	// defer f.Close()
+
+	// This parses the flags and sets the correct/given corresponding values.
+	flag.Parse()
+	fmt.Println(".:server is starting:.")
+
+	// launch the server
+	launchServer()
+
+	// code here is unreachable because launchServer occupies the current thread.
+}
+
+func launchServer() {
+	log.Printf("Server %s: Attempts to create listener on port %s\n", *serverName, *port)
+
+	// Create listener tcp on given port or default port 5400
+	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
+	if err != nil {
+		log.Printf("Server %s: Failed to listen on port %s: %v", *serverName, *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
+		return
+	}
+
+	// makes gRPC server using the options
+	// you can add options here if you want or remove the options part entirely
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	// makes a new server instance using the name and port from the flags.
+	server := &Server{
+		name:           *serverName,
+		port:           *port,
+		currentTime: 0, // gives default value, but not sure if it is necessary
+	}
+
+	gRPC.RegisterChittyChatServer(grpcServer, server) //Registers the server to the gRPC server.
+
+	log.Printf("Server %s: Listening at %v\n", *serverName, list.Addr())
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
+	// code here is unreachable because grpcServer.Serve occupies the current thread.
+}
+
+// The method format can be found in the pb.go file. If the format is wrong, the server type will give an error.
+func (s *Server) Subscribe(ctx context.Context, SubMessage *gRPC.SubMessage) (*gRPC.ChatMessage, error) {
+	
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	
+	s.currentTime = int64(time.Now().Nanosecond())
+	return &gRPC.ChatMessage{ServerName: s.name,NanoSeconds: s.currentTime}, nil
+}
+
+// Get preferred outbound ip of this machine
+// Usefull if you have to know which ip you should dial, in a client running on an other computer
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
+// sets the logger to use a log.txt file instead of the console
+func setLog() *os.File {
+	// Clears the log.txt file when a new server is started
+	if err := os.Truncate("log.txt", 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+
+	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
+}
